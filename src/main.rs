07ec33e25f85b3ebccc96205ab8ac7f433216dd5
HappyNano <akurzov41@gmail.com>
2023-01-04 21:15:28 +0300

    Removed create_person func
    1. Removed create_person function
    2. Now login create person if it does not exist

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     fs::File,
     sync::{Arc, Mutex},
 };
-use tide::Request;
+use tide::{Request, new};
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -94,12 +94,11 @@ async fn main() -> tide::Result<()> {
     // });
 
     app.at("/reset").get(quit);
-    app.at("/create_user").post(create_person);
+    app.at("/login").post(login);
 
     app.at("/").get(index);
     app.at("/terminate").get(|request: tide::Request<Arc<Mutex<DataBase>>>| async move {
         let state = request.state();
-
         std::fs::write("data.base", serde_json::to_string(state).unwrap().as_bytes())?;
         std::process::exit(0);
         #[allow(unreachable_code)]
@@ -112,12 +111,7 @@ async fn main() -> tide::Result<()> {
     Ok(())
 }
 
-async fn index(req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
-    let visits: usize = req.session().get("visits").unwrap();
-    Ok(format!("you have visited this website {} times", visits).into())
-}
-
-async fn create_person(mut req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
+async fn login(mut req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
     #[derive(serde::Serialize, serde::Deserialize)]
     struct TmpPerson {name: String}
     let tmp_person: TmpPerson = req.body_json().await?;
@@ -126,29 +120,51 @@ async fn create_person(mut req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
         return Ok(tide::Redirect::new("/").into());
     }
 
-    let state = req.state();
-    let mut guard = state.lock().unwrap();
-
-    match guard.users.values().find(
-        |p| p.name == tmp_person.name
-    ) {
-        None => {}
-        _ => {
-            println!("This user exist!");
-            return Ok(tide::Redirect::new("/").into());
-        },
-    };
-
-    let person = Person {
-        name: tmp_person.name,
-        santa_to: String::new(),
-        in_group: -1,
-        access: Access::User
-    }; 
-    let new_id: i8 = guard.users.len() as i8; 
-    guard.users.insert(new_id, person);
+    let user_id: i8;
+    
+    {
+        let state = req.state();
+        let mut guard = state.lock().unwrap();
+        match guard.users.iter().find(
+            |p| p.1.name == tmp_person.name
+        ) {
+            None => {
+                let person = Person {
+                    name: tmp_person.name,
+                    santa_to: String::new(),
+                    in_group: -1,
+                    access: Access::User
+                }; 
+                let new_id: i8 = guard.users.len() as i8; 
+                guard.users.insert(new_id, person);
+                user_id = new_id;
+            }
+            Some(p) => {
+                user_id = p.0.clone();
+            },
+        };
+    }
+    
+    let session = req.session_mut();
+    session.insert("user_id", user_id)?;
     
     Ok(tide::Redirect::new("/").into())
+}
+
+async fn index(req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
+    let user_id: i8 = req.session().get("user_id").unwrap();
+    if user_id == -1
+    {
+        return Ok("You have to log in using POST request on /login with your name".into());
+    }
+    let state = req.state();
+    let guard = state.lock().unwrap();
+    let user = guard.users.iter().find(
+        |p| {
+            p.0 == &user_id
+        });
+    let user_name = &user.unwrap().1.name;
+    Ok(format!("Hello {user_name}!").into())
 }
 
 async fn quit(mut req: Request<Arc<Mutex<DataBase>>>) -> tide::Result {
